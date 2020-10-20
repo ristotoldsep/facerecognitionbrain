eddeb2beca68b3529dd6e06f4a3fb39c24f81c4b
Risto Tõldsep <r.toldsep@gmail.com>
2020-10-21 01:45:30 +0300

    Added logo and search bar component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
+import ImageLinkForm from './components/imagelinkform/ImageLinkForm';
 import './App.css';
 
 
@@ -8,10 +9,10 @@ class App extends Component {
   render() {
     return (
         <div className="App">
-            <Navigation />
-            <Logo />
-           {/*  <ImageLinkForm />
-            <FaceRecognition /> */}
+          <Navigation />
+          <Logo />
+          <ImageLinkForm />
+            {/* <FaceRecognition /> */}
         </div>
     );
   }
