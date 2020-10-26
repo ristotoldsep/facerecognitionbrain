5babd16da2badb0306faa9e2bdf9c1761468f167
Risto Tõldsep <r.toldsep@gmail.com>
2020-10-26 02:08:08 +0200

    Completed front-end, added sign in functionality and made img recognition work

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 import Navigation from './components/navigation/Navigation';
+import SignIn from './components/signin/SignIn';
+import Register from './components/register/Register';
 import Logo from './components/logo/Logo';
 import Rank from './components/rank/Rank';
 import ImageLinkForm from './components/imagelinkform/ImageLinkForm';
@@ -31,47 +33,76 @@ class App extends Component {
     this.state = {
       input: '',
       imageUrl: '',
-      box: {}
+      box: {},
+      route: 'signin',
+      isSignedIn: false
     }
   }
 
-calculateFaceLocation = (data) => {
-  const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-  const image = document.getElementById('inputimage');
-  const width = Number(image.width);
-  const height = Number(image.height);
-  // console.log(width, height);
-  return {
-    leftCol: clarifaiFace.left_col * width;
-    topRow: clarifaiFace.top_row * height;
-    rightCol: width - (clarifaiFace.right_col * width);
-    bottomRow: height - (clarifaiFace.bottom_row * height);
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    // console.log(width, height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
   }
-}
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
+  }
 
   // PARAMETER OF APP
   onInputChange = (event) => {
     this.setState({input: event.target.value});
   }
 
+  //HERE I CAN CHANGE MODEL ID
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
     app.models
     .predict('c0c0ac362b03416da06ab3fa36fb58e3', this.state.input)
-    .then(response => this.calculateFaceLocation(response))
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
     .catch(err => console.log(err));
   }
+
+  onRouteChange = (route) => {
+    if(route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if(route === 'home') {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
+  }
+
   render() {
+    const { isSignedIn, imageUrl, route, box } = this.state; //Destructuring/refactoring codE!
     return (
       //COMPONENTS
         <div className="App">
         <Particles className="particles"
         params={particlesOptions} />
-          <Navigation />
-          <Logo />
-          <Rank />
-          <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
-          <FaceRecognition imageUrl={this.state.imageUrl}/>
+          <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+          { route === 'home'
+            ? <div>
+                <Logo />
+                <Rank />
+                <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+                <FaceRecognition box={box} imageUrl={imageUrl}/>
+              </div>
+            : (
+              route === 'signin' 
+              ? <SignIn onRouteChange={this.onRouteChange} /> //if not signed in, show form, if signed in, show content!  
+              : <Register onRouteChange={this.onRouteChange} />
+            ) 
+            
+          }
         </div>
     );
   }
